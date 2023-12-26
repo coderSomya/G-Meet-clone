bfd0e22c97596456819f82532e816dca26a9680d
Somya <isomya13@gmail.com>
2023-12-27 03:09:37 +0530

    added axios instance with jwt handler

---
+++ frontend/src/utils/Axios.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+
+const Axios = axios.create({
+    baseURL: import.meta.env.BASE_URL
+})
+
+const token = localStorage.getItem('token')
+
+if(token){
+    Axios.defaults.headers.common['Authorization'] = `JWT ${localStorage.getItem('token')}`
+}
+
+export default Axios;
